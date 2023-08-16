578f2172dc243e47d5bb4f89f68159ff95125c2d
EthanLiu <ethan.liu@presslogic.com>
2023-08-16 16:40:49 +0800

    feat: integrate microservices

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { EventPattern, MessagePattern } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,15 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @MessagePattern('math:wordcount')
+  wordCount(text: string): { [key: string]: number } {
+    return this.appService.calculateWordCount(text);
+  }
+
+  @EventPattern('math:wordcount_log')
+  wordCountLog(text: string): void {
+    console.log(text, '基于事件的传输方式');
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+
+const APP_ENV = 9010;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  // const app = await NestFactory.create(AppModule);
+  // await app.listen(3000);
+
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        // host: '127.0.0.1',
+        host: '0.0.0.0',
+        port: APP_ENV,
+        // tlsOptions: {
+        //   host: '0850-103-43-162-133.ngrok.io',
+        // },
+      },
+    },
+  );
+  // app.listen(() => console.log('Microservice is listening')); 此处代码会报错
+  app.listen();
 }
 bootstrap();
